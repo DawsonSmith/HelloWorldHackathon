04165015390b1a78093b4b2d83cc498e873f50e8
jman085 <42813147+jman085@users.noreply.github.com>
2018-09-15 19:36:16 -0400

    Schedule changes
    put in exercises

--- src/Exercise.java
+++ src/Exercise.java
@@ -41,6 +41,7 @@ public class Exercise {
 		}
 		
 	}
+
 	public double getPersonalBest() {
 		return personalBest;
 	}
--- src/Schedule.java
+++ src/Schedule.java
@@ -1,4 +1,27 @@
 
 public class Schedule {
-	
+	String[] benchPressTargets = {"Chest","Triceps","Shoulders"};
+	Exercise benchPress = new Exercise("Barbell Bench Press", "Lower bar to chest then push weight back up while keeping butt on the bench",benchPressTargets);
+	String[] highBarSquatTargets = {"Quadriceps","Hamstrings","Calves"};
+	Exercise highBarSquat = new Exercise("High Bar Squat", "Load barbell on traps and squat down to parallel, then push feet through ground to lift weight back up", highBarSquatTargets);
+	String[] deadliftTargets= {"Back","Hamstrings"};
+	Exercise deadlift = new Exercise("Barbell Deadlift","With feet positioned shoulder width apart and underneath the bar, grip bar slightly outside shoulder width and pull weight off ground while keeping back straight", deadliftTargets);
+	String[] dumbbellShoulderPressTargets = {"Shoulders"};
+	Exercise dumbbellShoulderPress = new Exercise("Dumbbell Shoulder Press","With two dumbbells of equivalent weight, either standing or sitting, lift weight vertically above head while keeping back straight", dumbbellShoulderPressTargets);
+	String[] dipsTargets = {"Triceps", "Chest"};
+	Exercise dips = new Exercise("Dips", "With hands on bars approximately shoulder width apart, slowly lower yourself downwards untl elbows are at a 90 degree angle, then push back up until arms are locked out", dipsTargets);
+	String[] cableFlyTargets = {"Chest","Shoulders"};
+	Exercise cableFly = new Exercise("Chest Cable Fly","Holding cables in both hands at a heigth equivalent to chest, slowly bring arms forward, bringing hands together as if you were clapping",cableFlyTargets);
+	String[] lateralDeltRaisesTargets = {"Shoulders (Side Deltoids)"};
+	Exercise lateralDeltRaises = new Exercise("Lateral Delt Raises", "With a dumbbell in each hand, slowly raise arms to the side as if T-posing, then slowly lower them back to side",lateralDeltRaisesTargets);
+	String[] frontDeltRaisesTargets = {"Shoulders (Front Deltoids)"};
+	Exercise frontDeltRaises = new Exercise("Front Delt Raise","With dumbbell in each hand, slowly raise arms in front of body until arms are parallel to the ground, then slowly lower weight back down to your side", frontDeltRaisesTargets);
+	String[] tricepExtensionTargets = {"Triceps"};
+	Exercise tricepExtension = new Exercise("Tricep Extension","Using a rope attachment, grip bottom of rope and, while keeping elbows locked in place, slowly pull the rope towards the gound until arms are locked out. Afterwards slowly return to start position", tricepExtensionTargets);
+	String[] skullcrushTargets = {"Triceps"};
+	Exercise skullcrush = new Exercise("Tricep Skullcrush Extension", "While laying on bench with arms locked out above body holding a barbell, slowly bend elbows until barbell is in contact with forehead, then push bar back up to starting position", skullcrushTargets);
+	String[] inclineDumbbellBenchTargets = {"Chest","Shoulders","Triceps"};
+	Exercise inclineDumbbellBench = new Exercise("Incline Dumbbell Bench", "Using dumbbells on an incline bench, push dumbbells upwards until arms are locked out then back down to chest", inclineDumbbellBenchTargets);
+	String[] pullupTargets = {"Back","Bicep"};
+	Exercise pullup = new Exercise("Pullup","In a dead-hang with ")
 }
