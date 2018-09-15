8c6dc76e379a7bf54f4e133fbf3aba50a6a6867d
ShariganKon <36146015+ShariganKon@users.noreply.github.com>
2018-09-15 12:14:12 -0400

    This shit retarded as hell
    Boi this mothafucka bouta be lit as hell

--- src/FitnessApp.java
+++ src/FitnessApp.java
@@ -13,13 +13,71 @@
  * 
  */
 
-public class FitnessApp 
-{
+import java.util.Scanner;
+public class FitnessApp {
 
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		//Output:
+		//Body Fat Percentage of Person
+		//Body Mass Index
+		//Range of Body Fat Percentage (Athletic, Fit, Acceptable, or Obese range)
+		//Exercise with intensity
+		//
+		//
+		Scanner input = new Scanner(System.in);
+		String sex;
+		double weight;
+		double wristCircumference;
+		double waistCircumference;
+		double forearmCircumference;
+		double hipCircumference;
+		double bodyFatPercentage;
+		double bodyMassIndex;
+		double factor1;
+		double factor2;
+		double factor3;
+		double factor4;
+		double factor5;
+		double leanBodyMass;
+		double bodyFatWeight;
+		double height;
+
+		System.out.println("Welcome to Your Personal Fitness Application");
+		System.out.println("Please Enter Your Gender (Male or Female): ");
+		sex = input.next();
+		System.out.println("Please Enter your Height in Inches: ");
+		height = input.nextDouble();
+		System.out.println("Please Enter Your Weight in Pounds: ");
+		weight = input.nextDouble();
+		System.out.println("Please Enter Your Wrist Circumference in Inches (At Fullest Point) : ");
+		wristCircumference = input.nextDouble();
+		System.out.println("Please Enter Your Waist Circumference in Inches (At Naval) : ");
+		waistCircumference = input.nextDouble();
+		System.out.println("Please Enter Your Forearm Circumference in Inches (At Fullest Point) : ");
+		forearmCircumference = input.nextDouble();
+		System.out.println("Please Enter Your Hip Circumference in Inches (At Fullest Point) : ");
+		hipCircumference = input.nextDouble();
+		if (sex .equals("Female"));
+		{
+			factor1 = (weight * .732) + 8.987;
+			factor2 = wristCircumference / 3.140;
+			factor3 = waistCircumference * .157;
+			factor4 = hipCircumference * .249;
+			factor5 = forearmCircumference * .434;
+			leanBodyMass = factor1 + factor2 - factor3 - factor4 + factor5;
+			bodyFatWeight = weight - leanBodyMass;
+			bodyFatPercentage = (bodyFatWeight * 100) / weight;
+		}
+		if (sex .equals("Male")){
+
+		}
+		factor1 = (weight * 1.082) + 94.42;
+		factor2 = waistCircumference * 4.15;
+		leanBodyMass = factor1 - factor2;
+		bodyFatWeight = weight - leanBodyMass;
+		bodyFatPercentage = (bodyFatWeight * 100)/weight;
 
 	}
+
 
 }
