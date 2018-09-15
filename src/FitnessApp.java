eca6118c0a4974d30a4d39cc9a23dc469edb3a96
Dawson Smith <dawsonsmith64@yahoo.com>
2018-09-15 12:02:39 -0400

    Setup main method
    beginning of project setup

---
+++ src/FitnessApp.java
@@ -0,0 +1,25 @@
+
+/*
+ * 			PURDUE HELLO WORLD FRESHAMN HACKATHON
+ * 		- PHIT -
+ * 			an Android application to help people get into physical fitness
+ * 
+ * 		- CONTRIBUTERS -
+ * 			Ranjan Behl
+ * 			Mykyta Finchenko
+ * 			Jarrett Ford
+ * 			Andrew Gladden
+ * 			Dawson Smith
+ * 
+ */
+
+public class FitnessApp 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
