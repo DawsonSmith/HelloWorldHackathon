6ddd0533beda780cd55b88e66f457607a0c48323
Dawson Smith <dawsonsmith64@yahoo.com>
2018-09-15 14:17:05 -0400

    Organized prior code into an object
    Put Mykyta's code into an object using the GenerateUser class. This will
    allow for ease of use when having multiple users.

--- src/FitnessApp.java
+++ src/FitnessApp.java
@@ -14,14 +14,21 @@
  */
 
 import java.util.Scanner;
+
 public class FitnessApp {
 
 	public static void main(String[] args) {
-		//Output:
-		//Body Fat Percentage of Person
-		//Body Mass Index
-		//Range of Body Fat Percentage (Athletic, Fit, Acceptable, or Obese range)
-		//Exercise with intensity
+		//Scanner input = new Scanner(System.in);
+		GenerateUser user = new GenerateUser();
+		System.out.println(user.getWeight());
+	}
+	/*
+	public static double[] generateUser() {
+		// Output:
+		// Body Fat Percentage of Person
+		// Body Mass Index
+		// Range of Body Fat Percentage (Athletic, Fit, Acceptable, or Obese range)
+		// Exercise with intensity
 		//
 		//
 		Scanner input = new Scanner(System.in);
@@ -42,7 +49,6 @@ public class FitnessApp {
 		double bodyFatWeight;
 		double height;
 
-
 		System.out.println("Welcome to Your Personal Fitness Application");
 		System.out.println("Please Enter Your Gender (Male or Female): ");
 		sex = input.next();
@@ -58,7 +64,8 @@ public class FitnessApp {
 		forearmCircumference = input.nextDouble();
 		System.out.println("Please Enter Your Hip Circumference in Inches (At Fullest Point) : ");
 		hipCircumference = input.nextDouble();
-		if (sex .equals("Female"));
+		if (sex.equals("Female"))
+			;
 		{
 			factor1 = (weight * .732) + 8.987;
 			factor2 = wristCircumference / 3.140;
@@ -68,19 +75,18 @@ public class FitnessApp {
 			leanBodyMass = factor1 + factor2 - factor3 - factor4 + factor5;
 			bodyFatWeight = weight - leanBodyMass;
 			bodyFatPercentage = (bodyFatWeight * 100) / weight;
-			bodyMassIndex = (weight * 703)/(height * height);
+			bodyMassIndex = (weight * 703) / (height * height);
 		}
-		if (sex .equals("Male")){
+		if (sex.equals("Male")) {
 
 		}
 		factor1 = (weight * 1.082) + 94.42;
 		factor2 = waistCircumference * 4.15;
 		leanBodyMass = factor1 - factor2;
 		bodyFatWeight = weight - leanBodyMass;
-		bodyFatPercentage = (bodyFatWeight * 100)/weight;
-		bodyMassIndex = (weight * 703)/(height * height);
-
+		bodyFatPercentage = (bodyFatWeight * 100) / weight;
+		bodyMassIndex = (weight * 703) / (height * height);
+		return {weight, height};
 	}
-
-
+	*/
 }
---
+++ src/GenerateUser.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+public class GenerateUser {
+	Scanner input = new Scanner(System.in);
+	public boolean sex = true;
+	public double weight = 0.0;
+	public double wristCircumference = 0.0;
+	public double waistCircumference = 0.0;
+	public double forearmCircumference = 0.0;
+	public double hipCircumference = 0.0;
+	public double height = 0.0;
+	public GenerateUser() {
+		boolean sex = true;
+		double weight = 0.0;
+		double wristCircumference = 0.0;
+		double waistCircumference = 0.0;
+		double forearmCircumference = 0.0;
+		double hipCircumference = 0.0;
+		double height = 0.0;
+	}
+	public GenerateUser(Scanner s) {
+		boolean sex = true;
+		double weight = 0.0;
+		double wristCircumference = 0.0;
+		double waistCircumference = 0.0;
+		double forearmCircumference = 0.0;
+		double hipCircumference = 0.0;
+		double height = 0.0;
+		System.out.println("Welcome to Your Personal Fitness Application");
+		System.out.println("Please Enter Your Gender (Male or Female): ");
+		//sex = input.next();
+		System.out.println("Please Enter your Height in Inches: ");
+		height = input.nextDouble();
+		System.out.println("Please Enter Your Weight in Pounds: ");
+		weight = input.nextDouble();
+		System.out.println("Please Enter Your Wrist Circumference in Inches (At Fullest Point) : ");
+		wristCircumference = input.nextDouble();
+		System.out.println("Please Enter Your Waist Circumference in Inches (At Naval) : ");
+		waistCircumference = input.nextDouble();
+		System.out.println("Please Enter Your Forearm Circumference in Inches (At Fullest Point) : ");
+		forearmCircumference = input.nextDouble();
+		System.out.println("Please Enter Your Hip Circumference in Inches (At Fullest Point) : ");
+		hipCircumference = input.nextDouble();
+	}		
+	public double[] determineFactors(boolean sex, double weight, double waistCircumference, double wristCircumference, double hipCircumference, double forearmCircumference) {
+		if(sex) {
+			double[] ret = {(weight * 1.082) + 94.42, waistCircumference * 4.15};
+			return ret;
+		}else {
+			double[] ret = {(weight * .732) + 8.987, wristCircumference / 3.140, waistCircumference * .157, hipCircumference * .249, forearmCircumference * .434};
+			return ret;
+		}
+		
+	}
+	public double determineLeanBodyMass(boolean sex, double[] a) {
+		if(sex){
+			return a[0] - a[1];
+		}else{
+			return a[0] + a[1] - a[2] - a[3] + a[4];
+		}
+	}
+	public double determineBodyFatWeight(double leanBodyMass, double weight) {
+		return weight - leanBodyMass;
+	}
+	public double determineBodyFatPercent(double bodyFatWeight, double weight) {
+		return (bodyFatWeight * 100)/weight;
+	}
+	public double determineBMI(double weight, double height) {
+		return (weight * 703)/(height * height);
+	}
+	public String getSex() {
+		return sex? "Male":"Female";
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public double getWaistCircumference() {
+		return waistCircumference;
+	}
+	public double getForearmCircumference() {
+		return forearmCircumference;
+	}
+	public double getWristCircumference() {
+		return wristCircumference;
+	}
+	public double getHipCircumference() {
+		return hipCircumference;
+	}
+	public void setSex(boolean s) {
+		sex = s;
+	}
+	public void setWeight(double w) {
+		weight = w;
+	}
+	public void setHeight(double h) {
+		height = h;
+	}
+	public void setWaistCircumference(double wc) {
+		waistCircumference = wc;
+	}
+	public void setForearmCircumference(double fc) {
+		forearmCircumference = fc;
+	}
+	public void setWristCircumference(double wc) {
+		wristCircumference = wc;
+	}
+	public void setHipCircumference(double hc) {
+		hipCircumference = hc;
+	}
+}
