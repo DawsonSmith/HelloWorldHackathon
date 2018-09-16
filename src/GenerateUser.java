771bd8fb72a46cead0a05f9664e7e568a98172f7
jman085 <42813147+jman085@users.noreply.github.com>
2018-09-16 00:57:54 -0400

    idk

--- src/GenerateUser.java
+++ src/GenerateUser.java
@@ -104,50 +104,110 @@ public class GenerateUser {
 	public void setHipCircumference(double hc) {
 		hipCircumference = hc;
 	}
-	public void determineHealth() {
-		//point-slope form for height which is compare in this statement
-		double compare = (12.0/42.0)*(weight-98)+61;
-		if(height>compare) {
-			return "You at at an unhealty range for your characteristics.";
-		}
-		compare = (11.0/56.0)*(weight-210)+77;
-		if(height>compare) {
-			return "You are at a heatlthy range for your characteristics.";
-		}
-		compare = (19.0/112.0)*(weight-266)+79;
-		if(height>compare) {
-			return "You are at an overweight range for your characteristics.";
-		}
-		return "You are at an obese range for your characteristics.";
-		if ()
-		if (sex = true){
-			if (determineBodyFatPercent()> 6 || determineBodyFatPercent() < 13){
-				return "Based on your body fat percentage, you are catagorized as athletic"
-			}
-			else if (determineBodyFatPercent() > 14 || determineBodyFatPercent() < 17){
-				return "Based on your body fat percentage, you are catagorized as fit"
-			}
-			else if (determineBodyFatPercent() > 18 || determineBodyFatPercent() < 25){
-				return "Based on your body fat percentage, you are catagorized as average"
-				}
-				else if (determineBodyFatPercent() >25){
-					return "Based on your body fat percentage, you are catagorized as obese"
-			}
-		}
-		else if (sex = false){
-			if(determineBodyFatPercent()> 14 || determineBodyFatPercent() <20){
-				return "Based on your body fat percentage, you are catagorized as athletic"
-			}
-			else if(determineBodyFatPercent()>21||determineBodyFatPercent()<24){
-				return "Based on your body fat percentage, you are catagorized as fit"
-			}
-			else if(determineBodyFatPercent()>25||determineBodyFatPercent()<31){
-				return "Based on your body fat percentage, you are catagorized as average"
-			}
-			else if(determineBodyFatPercent()>32){
-				return "Based on your body fat percentage, you are catagorized as obese"
-			}
-		}
 
+	public boolean isUnderweight() {
+		double compare = (12.0/42.0)*(weight-98)+61;
+		if(height>compare)
+			return true;
+		return false;
+	}
+	public boolean isHealthyWeight() {
+		double compare =  (11.0/56.0)*(weight-210)+77;
+		if(height>compare)
+			return true;
+		return false;
+	}
+	public boolean isOverweight() {
+		double compare =  (19.0/112.0)*(weight-266)+79;
+		if(height>compare)
+			return true;
+		return false;
+	}
+	public boolean isObese() {
+		if(!isUnderweight() && !isHealthyWeight() && !isOverweight())
+			return true;
+		return false;
+	}
+
+	//*
+	//  public void determineHealth() {
+	//	point-slope form for height which is compare in this statement
+	//		double compare = (12.0/42.0)*(weight-98)+61;
+	//		if(height>compare) {
+	//			return "You at at an underweight range for your characteristics.";
+	//		}
+	//		compare = (11.0/56.0)*(weight-210)+77;
+	//		if(height>compare) {
+	//			return "You are at a heatlthy range for your characteristics.";
+	//		}
+	//		compare = (19.0/112.0)*(weight-266)+79;
+	//		if(height>compare) {
+	//			return "You are at an overweight range for your characteristics.";
+	//		}
+	//		return "You are at an obese range for your characteristics.";
+	//	}
+		public boolean isAthletic() {
+			if(sex)
+				if (determineBodyFatPercent()> 6 && determineBodyFatPercent() < 13)
+					return true;
+			if(!sex)
+				if(determineBodyFatPercent()> 14 || determineBodyFatPercent() <20)
+					return true;
+			return false;
+		}
+		public boolean isFit() {
+			if(sex)
+				if (determineBodyFatPercent() > 14 && determineBodyFatPercent() < 17)
+					return true;
+			if(!sex)
+				if(determineBodyFatPercent()>21||determineBodyFatPercent()<24)
+					return true;
+			return false;
+		}
+		public boolean isAverage() {
+			if(sex)
+				if (determineBodyFatPercent() > 18 && determineBodyFatPercent() < 25)
+					return true;
+			if(!sex)
+				if(determineBodyFatPercent()>25||determineBodyFatPercent()<31)
+					return true;
+			return false;
+		}
+		public boolean isOverweight() {
+			if(sex)
+				if (determineBodyFatPercent() >25)
+					return true;
+			if(!sex)
+				if (determineBodyFatPercent()>32)
+					return true;
+			return false;
+		}
+	}
+
+
+
+		//*
+// if (sex = true){
+//			if (determineBodyFatPercent()> 6 || determineBodyFatPercent() < 13)
+//				return "Based on your body fat percentage, you are catagorized as athletic"
+//			else if (determineBodyFatPercent() > 14 || determineBodyFatPercent() < 17)
+//				return "Based on your body fat percentage, you are catagorized as fit"
+//			else if (determineBodyFatPercent() > 18 || determineBodyFatPercent() < 25)
+//				return "Based on your body fat percentage, you are catagorized as average"
+//			else if (determineBodyFatPercent() >25){
+//					return "Based on your body fat percentage, you are catagorized as obese"
+//			}
+//		}
+//		else if (sex = false){
+//			if(determineBodyFatPercent()> 14 || determineBodyFatPercent() <20)
+//				return "Based on your body fat percentage, you are catagorized as athletic"
+//			else if(determineBodyFatPercent()>21||determineBodyFatPercent()<24)
+//				return "Based on your body fat percentage, you are catagorized as fit"
+//			else if(determineBodyFatPercent()>25||determineBodyFatPercent()<31)
+//				return "Based on your body fat percentage, you are catagorized as average"
+//			else if(determineBodyFatPercent()>32)
+//				return "Based on your body fat percentage, you are catagorized as obese"
+//
+//
 	}
 }
